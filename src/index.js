f267457eebfc84819d27e2aa3ed2c5669235a7fd
Ayibatari Ibaba <tariibabadev@gmail.com>
2021-06-18 15:46:54 +0100

    Remove redundant if statement

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 let Service;
-if (['win32', 'darwin', 'linux'].includes(process.platform)) {
-  switch (process.platform) {
-    case 'win32':
-      Service = require('node-windows').Service;
-      break;
-    case 'darwin':
-      Service = require('node-mac').Service;
-      break;
-    case 'linux':
-      Service = require('node-linux').Service;
-      Object.defineProperty(Service.prototype, 'exists', {
-        writable: false,
-        get: Service.prototype.exists,
-      });
-      break;
-    default:
-      break;
-  }
+switch (process.platform) {
+  case 'win32':
+    Service = require('node-windows').Service;
+    break;
+  case 'darwin':
+    Service = require('node-mac').Service;
+    break;
+  case 'linux':
+    Service = require('node-linux').Service;
+    Object.defineProperty(Service.prototype, 'exists', {
+      writable: false,
+      get: Service.prototype.exists,
+    });
+    break;
+  default:
+    break;
 }
 
 module.exports = Service;
